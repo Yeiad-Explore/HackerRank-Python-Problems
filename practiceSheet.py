ea0b49f1f1d1402fe76735f4ab152a469ece9953
yeiad <kabidyeiad101@gmail.com>
2022-09-26 09:03:10 +0600

    added

--- practiceSheet.py
+++ practiceSheet.py
@@ -40,6 +40,26 @@
 # lst.remove(lst[1])
 
 # print(lst)
-s = 'hello'
-for i in range(0, len(s)):
-    print (s[i])
+# s = 'hello'
+# for i in range(0, len(s)):
+#     print (s[i])
+
+# thickness = int(input()) #This must be an odd number
+# c = 'H'
+
+# #Top Cone
+# for i in range(thickness):
+#     print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+# #Top Pillars
+# for i in range(thickness+1):
+#     print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+# #Middle Belt
+# for i in range((thickness+1)//2):
+#     print((c*thickness*5).center(thickness*6))        
+#     #Bottom Pillars
+# for i in range(thickness+1):
+#     print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))   
+
+#     #Bottom Cone
+# for i in range(thickness):
+#     print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
